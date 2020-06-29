e1f808caf1a0c0f3e665f19e34c8a33a1432fa3f
lider123 <k_babaec@mail.ru>
2020-06-29 11:27:12 +0500

    Added database

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from firebase_admin import auth, credentials
 from firebase_admin._auth_utils import InvalidIdTokenError
 import firebase_admin
 import os
+import pymysql
 
 load_dotenv()
 app = Bottle()
@@ -13,12 +14,15 @@ app.install(LoggingPlugin(app.config))
 
 @app.get("/api/profile")
 def get_profile():
-    return {  # TODO: return real one
-        "id": 1,
-        "nickname": "johndoe",
-        "first_name": "John",
-        "last_name": "Doe"
-    }
+    token = request.headers.get("Authorization")
+    if token is None:
+        response.status = 401
+        return "Unauthorized"
+    profile = get_profile_from_db(token)
+    if profile is None:
+        profile = create_user(token)
+        response.status = 201
+    return profile
 
 
 @app.post("/api/authorization")
@@ -33,6 +37,45 @@ def authorize():
     except InvalidIdTokenError:
         response.status = 510
         return "Invalid auth token"
+
+
+def get_profile_from_db(token):
+    connection = create_connection()
+    try:
+        with connection.cursor() as cursor:
+            query = f"""
+            SELECT user_id, nickname, first_name, last_name
+            FROM users
+            WHERE firebase_token = '{token}'
+            """
+            cursor.execute(query)
+            result = cursor.fetchone()
+            return result
+    finally:
+        connection.close()
+
+
+def create_user(token):
+    connection = create_connection()
+    try:
+        with connection.cursor() as cursor:
+            query = f"""
+            INSERT INTO users (firebase_token) VALUES ('{token}')
+            """
+            cursor.execute(query)
+        connection.commit()
+    finally:
+        connection.close()
+    return get_profile_from_db(token)
+
+
+def create_connection():
+    return pymysql.connect(host="localhost",
+                           user="admin",
+                           password="admin",
+                           db="muspert",
+                           charset="utf8mb4",
+                           cursorclass=pymysql.cursors.DictCursor)
 
 
 if __name__ == "__main__":
